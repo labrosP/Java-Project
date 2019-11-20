9c62f0d8121fa2745a3415d2571e1d5ff3972b97
Maria <mariamakantasi@gmail.com>
2019-11-20 19:41:53 +0200

    configure spring security and make h2 console reachable from browser

---
+++ src/main/java/com/persado/assignment/project/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.persado.assignment.project;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ * @author maria
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic().disable();
+
+        //h2 console to be accessible from browser
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+    }
+
+}
