62a5f3097a662639c8cc1d4d7d8b42a891d33591
Maria <mariamakantasi@gmail.com>
2019-11-23 22:09:35 +0200

    validation messages css and js

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,7 +2,17 @@ $(document).ready(function () {
   $("#menu").load("/menu.html");
 });
 
-$('.nav li').click(function(){
-    $('.nav li').removeClass('active');
-    $(this).addClass('active');
+$('.nav li').click(function () {
+  $('.nav li').removeClass('active');
+  $(this).addClass('active');
 })
+
+
+function confirmDialog(text) {
+  var retVal = confirm(text);
+  if (retVal == true) {
+    return true;
+  } else {
+    return false;
+  }
+};
